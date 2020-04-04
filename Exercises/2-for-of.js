6ba1ba4857d35a8a235a92979951f6cf2794c333
sairin <siriel88@gmail.com>
2020-04-04 23:16:20 +0300

    Work complete!

--- Exercises/2-for-of.js
+++ Exercises/2-for-of.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let sum = 0;
+  for (const num of args) {
+    sum += num;
+  }
+  return sum;
 };
+
+const a = sum(1, 2, 3); // a === 6
+const b = sum(0); // b === 0
+const c = sum(); // c === 0
+const d = sum(1, -1, 1); // d === 1
+const e = sum(10, -1, -1, -1); // e === 7
+
+console.log({ a, b, c, d, e });
 
 module.exports = { sum };
--- Exercises/4-do-while.js
+++ Exercises/4-do-while.js
@@ -1,9 +1,24 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use do..while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  if (args.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  do {
+    sum += args.pop();
+  } while (args.length > 0);
+  return sum;
+
 };
+
+const a = sum(1, 2, 3); // a === 6
+const b = sum(0); // b === 0
+const c = sum(); // c === 0
+const d = sum(1, -1, 1); // d === 1
+const e = sum(10, -1, -1, -1); // e === 7
+
+console.log({ a, b, c, d, e });
 
 module.exports = { sum };
